8cecdbade50af05ee38bf11134dcb5f134d367cf
Fernando Fernández Valero <ferval19@gmail.com>
2018-09-25 11:51:31 +0200

    -Ejercicos DOM y arrays

---
+++ ejemplos/js/arrays.js
@@ -0,0 +1,10 @@
+'use strict'
+
+console.log('yeeeee');
+
+var numeros = new Array(6);
+
+for (var i = 0; i < 6; i++) {
+    numeros.push(parseInt(prompt('Introduce numero: ', 0)));
+}
+console.log(numeros);
---
+++ ejemplos/js/dom.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//DOM: Document Object Model
+
+var caja = document.getElementById('caja');
+
+caja.style.background = 'red';
+
+console.log(caja);
